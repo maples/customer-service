386f1016c34243617151c3f52ec1677c3e7354ed
Flavio Luiz Mendes <flmendes@gmail.com>
2022-12-02 17:29:01 +0000

    fix code smell

--- src/main/java/org/acme/customer/CustomerEntity.java
+++ src/main/java/org/acme/customer/CustomerEntity.java
@@ -17,5 +17,31 @@ public class CustomerEntity extends BaseEntity {
     @Column( name = "PHONE")
     public String phoneNumber;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CustomerEntity other = (CustomerEntity) obj;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+
+    
 
 }
